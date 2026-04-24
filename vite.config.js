6bc2611b72a76d78ffff79f00cc64b3dd6708436
李宗祐 <1>
2026-04-24 20:50:11 +0800

    Enable privacy policy page in Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
+
 export default defineConfig({
   plugins: [],
   server: {
     port: 5173
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        privacyPolicy: resolve(__dirname, 'privacy-policy/index.html')
+      }
+    }
   }
 })
